afe331eb260d4a0219c9390fc7b51764960cfae9
Corentin Gay <corentin.gay@epita.fr>
2017-08-10 12:48:56 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#TODO add xinitrc in there
+FILES=(vimrc bashrc muttrc)
+
+CURR_FILE=lol
+
+for i in ${!FILES[@]}; do
+	CURR_FILE=${FILES[i]}
+	echo config_files/$CURR_FILE
+	echo $HOME/.$CURR_FILE
+#	ln -is configs_files/$CURR_FILE $HOME/.$CURR_FILE
+done
+
