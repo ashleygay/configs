ac79774563046fc75d56bd5d2a18b20536807800
Corentin Gay <corentin.gay@epita.fr>
2017-08-10 12:52:29 +0200

    fixing dirs problems

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ CURR_FILE=lol
 
 for i in ${!FILES[@]}; do
 	CURR_FILE=${FILES[i]}
-	echo config_files/$CURR_FILE
+	echo ./config_files/$CURR_FILE
 	echo $HOME/.$CURR_FILE
-#	ln -is configs_files/$CURR_FILE $HOME/.$CURR_FILE
+#	ln -is ./configs_files/$CURR_FILE $HOME/.$CURR_FILE
 done
 
